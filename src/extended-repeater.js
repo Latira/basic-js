1d348d924894b2cb3fb881dd1ff8c5b2559a7f7d
Latira <l@g.com>
2025-01-06 18:24:35 +0300

    update: eighth task's solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = str + "";
+  function addition(add, repeatAdd, addSep) {
+    if (options.hasOwnProperty("addition")) {
+      if (options.hasOwnProperty("additionRepeatTimes")) {
+        for (let j = 1; j < repeatAdd; j++) {
+          result = result + add;
+          if (options.hasOwnProperty("additionSeparator")) {
+            result = result + addSep;
+          }  else {
+            result = result + "|";
+            console.log('addition repeat cycle + sep', result);
+          }
+          console.log('addition repeat cycle', result);
+        }
+      } 
+      result = result + options.addition;
+      console.log('addition cycle', result);
+    }
+  }
+  for (let i = 1; i < options.repeatTimes; i++) {
+    addition(options.addition, options.additionRepeatTimes, options.additionSeparator);
+    if (options.hasOwnProperty("separator")) {
+      result = result + options.separator + str;
+      console.log('separator cycle', result);
+    } 
+    else {
+      result = result + "+" + str;
+      console.log('cycle', result);
+    }
+  }
+  addition(options.addition, options.additionRepeatTimes, options.additionSeparator);
+  return result;
 }
 
 module.exports = {
