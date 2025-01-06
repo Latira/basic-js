569be166ccf7771653cc9bcf286860606d538b2d
Latira <l@g.com>
2025-01-06 10:32:03 +0300

    update: seventh task's solution

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  // let code = str.split('');
+  // let counts = new Map;
+  // let count;
+  // let result = "";
+  // for (let i = 0; i < code.length; i++) {
+  //   if (counts.has(code[i])) {
+  //     count = counts.get(code[i])
+  //     count++;
+  //     counts.set(code[i], count);
+  //   } else {
+  //     counts.set(code[i], 1);
+  //   }
+  // }
+  // counts.forEach((value, key, map) => {
+  //   result = result + value + key;
+  // });
+  // return result;
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i+1]) {
+      let n = 2;
+      while (str[i] === str [i+n]) {
+        n++;
+      }
+      result = result + n + str[i];
+      i = i + n - 1;
+    } else {
+      result = result + str[i];
+    }
+  }
+  return result;
 }
 
 module.exports = {
